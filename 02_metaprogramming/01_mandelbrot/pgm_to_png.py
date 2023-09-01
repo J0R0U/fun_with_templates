088f0b03dd765814f5aa8997407debe3d7f3aa2f
Jonas Rouven Schoenauer <y2s5a@unb.ca>
2023-09-01 11:26:19 -0300

    Change to MIT license using REUSE

--- 02_metaprogramming/01_mandelbrot/pgm_to_png.py
+++ 02_metaprogramming/01_mandelbrot/pgm_to_png.py
@@ -1,22 +1,5 @@
-# -----------------------------------------------------------------------------
-# 
-# This file is part of fun_with_templates.
-# 
-# fun_with_templates is free software: you can redistribute it and/or modify it
-# under the terms of the "Do What The Fuck You Want To Public License", Version
-# 2, as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
-# 
-# THE PROJECT "fun_with_templates" IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
-# KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
-# EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
-# OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH "fun_with_templates" OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-# 
-# SPDX-License-Identifier: WTFPL
-# 
-# -----------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2023 J0R0U <https://github.com/J0R0U>
+# SPDX-License-Identifier: MIT
 
 import cv2
 import sys
@@ -29,7 +12,7 @@ inFile = sys.argv[1]
 if len(sys.argv) > 2:
     outFile = sys.argv[2]
 else:
-    outFile = os.path.splitext(inFile)[0] + '.png'
+    outFile = os.path.splitext(inFile)[0] + ".png"
 
 # Read the input file
 img = cv2.imread(inFile)
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+<!--
+SPDX-FileCopyrightText: 2023 J0R0U <https://github.com/J0R0U>
+SPDX-License-Identifier: MIT
+-->
+
 # Readme
 
 ## Scope
